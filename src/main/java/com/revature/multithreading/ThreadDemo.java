ead96c050e3a0bcabdcdf650ff808c40380d9ded
Manikandan8056 <manikandanbala002@gmail.com>
2019-11-18 14:55:13 +0530

    MultiThread Example added

--- src/main/java/com/revature/multithreading/ThreadDemo.java
+++ src/main/java/com/revature/multithreading/ThreadDemo.java
@@ -4,21 +4,23 @@ public class ThreadDemo {
 
 	public static void main(String[] args) {
 
-		Count cnt = new Count();
+		Count cnt1 = new Count();
+		Count cnt2 = new Count();
 		
 		try {
-				cnt.start();
+				
+				cnt1.start();
+				cnt2.start();
 				System.out.println("Main thread will be alive till the child thread is live");
 				Thread.sleep(500);
 		}catch(InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
 
-
 class Count extends Thread {
+	
 	public void run() {
 		try {
 			for(int i = 0; i < 5; i++) {
